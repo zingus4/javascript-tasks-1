34c07841800c42ce3dbba4f074914aed0d9fab1e
zingus4 <zingus4@gmail.com>
2015-10-19 20:33:04 +0500

    +

--- roman-time.js
+++ roman-time.js
@@ -1,72 +1,39 @@
-var hours = process.argv[2];
-var minutes = process.argv[3];
+var hours = Number(process.argv[2]);
+var minutes = Number(process.argv[3]);
 var time="";
 
-if (!(isInteger(hours)) || !(isInteger(minutes)) || hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
+var higherNumbers = {
+    1: 'X',
+    2: 'XX',
+    3: 'XXX',
+    4: 'XL',
+    5: 'L'
+};
+var belowNumbers = {
+    0: '-',
+    1: 'I',
+    2: 'II',
+    3: 'III',
+    4: 'IV',
+    5: 'V',
+    6: 'VI',
+    7: 'VII',
+    8: 'VIII',
+    9: 'IX'
+};
+
+if (!isInteger(hours) || !isInteger(minutes) || hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
     console.log("Incorrect data: time isn't valid")
     process.exit(1);
 } 
 function isInteger(n) {
-    if (Number(n) == n && n % 1 === 0){
-        return true
-    }
-    return false
+    return (Number(n) == n && n % 1 === 0)
 }
-hours = Number(hours);
-minutes = Number(minutes);
-
-function returnRomanTime(mt) {
-    var t = ''
-    if (mt === 0){
+function returnRomanTime(data) {
+    if (data === 0){
         return "-"
     }
-    switch(Math.floor(mt / 10)) {
-        case 1:
-            t += "X"
-            break;
-        case 2:
-            t += "XX"
-            break;
-        case 3:
-            t += "XXX"
-            break;
-        case 4:
-            t += "XL"
-            break;
-        case 5:
-            t += "L"
-            break;
-    }
-    switch(mt % 10) {
-        case 1:
-            t += "I"
-            break;
-        case 2:
-            t += "II"
-            break;
-        case 3:
-            t += "III"
-            break;
-        case 4:
-            t += "IV"
-            break;
-        case 5:
-            t += "V"
-            break;
-        case 6:
-            t += "VI"
-            break;
-        case 7:
-            t += "VII"
-            break;
-        case 8:
-            t += "VIII"
-            break;
-        case 9:
-            t += "IX"
-            break;
-    }
-    return t;
+    return higherNumbers[Math.floor(data / 10)]+belowNumbers[Math.floor(data % 10)];
 }
 time = returnRomanTime(hours)+":"+returnRomanTime(minutes);
 console.log(time)
