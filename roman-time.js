0835159d56bb4102bf74b59c5a128883c104c618
zingus4 <zingus4@gmail.com>
2015-10-19 19:04:46 +0500

    New vertion

--- roman-time.js
+++ roman-time.js
@@ -2,104 +2,72 @@ var hours = process.argv[2];
 var minutes = process.argv[3];
 var time="";
 
-if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
-    console.log("Incorrect data: time is not true")
+if (!(isInteger(hours)) || !(isInteger(minutes)) || hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
+    console.log("Incorrect data: time isn't valid")
     process.exit(1);
 } 
+function isInteger(n) {
+    if (Number(n) == n && n % 1 === 0){
+        return true
+    }
+    return false
+}
+hours = Number(hours);
+minutes = Number(minutes);
 
-switch(Math.floor(hours / 10)) {
-	case 1:
-        time += "X";
-        break;
-	case 2:
-        time += "XX"
+function returnRomanTime(mt) {
+    var t = ''
+    if (mt === 0){
+        return "-"
+    }
+    switch(Math.floor(mt / 10)) {
+        case 1:
+            t += "X"
+            break;
+        case 2:
+            t += "XX"
+            break;
+        case 3:
+            t += "XXX"
+            break;
+        case 4:
+            t += "XL"
+            break;
+        case 5:
+            t += "L"
+            break;
+    }
+    switch(mt % 10) {
+        case 1:
+            t += "I"
+            break;
+        case 2:
+            t += "II"
+            break;
+        case 3:
+            t += "III"
+            break;
+        case 4:
+            t += "IV"
+            break;
+        case 5:
+            t += "V"
+            break;
+        case 6:
+            t += "VI"
+            break;
+        case 7:
+            t += "VII"
+            break;
+        case 8:
+            t += "VIII"
+            break;
+        case 9:
+            t += "IX"
+            break;
+    }
+    return t;
 }
-switch(hours % 10) {
-	case 1:
-        time += "I"
-        break;
-	case 2:
-        time += "II"
-        break;
-	case 3:
-        time += "III"
-        break;
-    case 4:
-        time += "IV"
-        break;
-	case 5:
-        time += "V"
-        break;
-	case 6:
-        time += "VI"
-        break;
-	case 7:
-        time += "VII"
-        break;
-	case 8:
-        time += "VIII"
-        break;
-    case 9:
-        time += "IX"
-        break;
-}
-if (hours == "0"){
-    time ="-"
-}
-
-time += ":"
-if (minutes == "0")
-{
-    time += "--"
-}
-else 
-{
-switch(Math.floor(minutes / 10)) {
-    case 1:
-        time += "X"
-        break;
-    case 2:
-        time += "XX"
-        break;
-    case 3:
-        time += "XXX"
-        break;
-    case 4:
-        time += "XL"
-        break;
-	case 5:
-        time += "L"
-        break;
-}
-switch(minutes % 10) {
-	case 1:
-        time += "I"
-        break;
-	case 2:
-        time += "II"
-        break;
-	case 3:
-        time += "III"
-        break;
-    case 4:
-        time += "IV"
-        break;
-	case 5:
-        time += "V"
-        break;
-	case 6:
-        time += "VI"
-        break;
-	case 7:
-        time += "VII"
-        break;
-	case 8:
-        time += "VIII"
-        break;
-    case 9:
-        time += "IX"
-        break;
-}
-}
+time = returnRomanTime(hours)+":"+returnRomanTime(minutes);
 console.log(time)
 
